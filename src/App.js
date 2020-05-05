a3811c4e3d955896bcddacf97ae503cf4cec9ac6
prabeshpathak <prabeshpathak50@gmail.com>
2020-05-05 16:39:52 +0545

    adding first react app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.components'
+import { SearchBox } from './components/search-box/search-box.components'
 
 class App extends Component{
   constructor(){
@@ -18,15 +19,23 @@ class App extends Component{
     .then(users => this.setState({monsters : users}));
   }
 
+  handleChanges= e =>{
+    this.setState({searchField : e.target.value});
+  };
+
   render(){
+    const {monsters , searchField} = this.state
+    const filteredMonster = monsters.filter( monster =>
+      monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+    )
   return (
     <div className="App">
-      <input
-       type="search"
-       placeholder="Search Monsters"
-       onChange={e=>
-         this.setState({searchField : e.target.value})}/>
-      <CardList monsters = {this.state.monsters}/>
+      <h1>Monster Rolodex</h1>
+      <SearchBox
+      placeholder= 'search monster'
+      handleChanges = {this.handleChanges}
+      />
+      <CardList monsters = {filteredMonster}/>
     </div>
   );
   }
