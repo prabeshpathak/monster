78822500e761b8d756356985e642d1864edadd14
prabeshpathak <prabeshpathak50@gmail.com>
2020-05-05 17:00:48 +0545

    Updates

---
+++ precache-manifest.e09bd835f54f5dd6f805ab632bfa04bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a432a0d277cacf0271a292cb7e2be560",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "1a79850e2addb092fab0",
+    "url": "/monster/static/css/main.f083c739.chunk.css"
+  },
+  {
+    "revision": "2803c25a1fe74d41dc51",
+    "url": "/monster/static/js/2.1ccc27c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster/static/js/2.1ccc27c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a79850e2addb092fab0",
+    "url": "/monster/static/js/main.ba50e8a4.chunk.js"
+  },
+  {
+    "revision": "c6a6b951baa2eeba648f",
+    "url": "/monster/static/js/runtime-main.0afc603b.js"
+  }
+]);
